ff820b8a93fe32fb413806df60acb7d22f2bbb35
Chamlee <arnaud8ruckebusch@gmail.com>
2016-02-07 12:10:52 +0100

    fix data display + data fetching

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -25,7 +25,7 @@ var CommentBox = React.createClass({
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList />
+                <CommentList data={this.props.data} />
                 <CommentForm />
             </div>
         );
@@ -37,10 +37,16 @@ var CommentBox = React.createClass({
  */
 var CommentList = React.createClass({
     render: function () {
+        var commentNodes = this.props.data.map(function (comment) {
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment>
+            );
+        });
         return (
             <div className="commentList">
-                <Comment author="Pete Hunt">This is one comment</Comment>
-                <Comment author="Jordan Walke">This is *another* comment</Comment>
+                { commentNodes }
             </div>
         );
     }
@@ -61,7 +67,6 @@ var CommentForm = React.createClass({
  * this.props => access to its commentList parent
  */
 var Comment = React.createClass({
-    //Protect from XSS attacks
     rawMarkup: function() {
         var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
         return { __html: rawMarkup };
@@ -73,7 +78,7 @@ var Comment = React.createClass({
                 <h2 className="commentAuthor">
                     { this.props.author }
                 </h2>
-                {marked(this.props.children.toString())}
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
             </div>
         );
     }
@@ -81,6 +86,6 @@ var Comment = React.createClass({
 
 
 ReactDOM.render(
-    < CommentBox />,
+    < CommentBox data={data}/>,
     document.getElementById('content')
 );
