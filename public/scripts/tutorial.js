51219c7ffe4ec5a07445f31a01f50e072d473d5b
Chamlee <arnaud8ruckebusch@gmail.com>
2016-02-07 18:22:07 +0100

    add ajax call to 'db'

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -11,13 +11,35 @@
  * CommentBox.js
  *
  * Look how CommentList and CommentForm are "injected"
+ * If state is updated the component update itself
  */
 var CommentBox = React.createClass({
+    getInitialState: function() {
+        return {data: []};
+    },
+    loadCommentsFromServer: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function (data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    //Automaticlly loaded
+    componentDidMount: function () {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
     render: function () {
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList data={this.props.data} />
+                <CommentList data={this.state.data} />
                 <CommentForm />
             </div>
         );
@@ -78,6 +100,6 @@ var Comment = React.createClass({
 
 
 ReactDOM.render(
-    < CommentBox url="/api/comments" />,
+    < CommentBox url="/api/comments" pollInterval={2000} />,
     document.getElementById('content')
 );
